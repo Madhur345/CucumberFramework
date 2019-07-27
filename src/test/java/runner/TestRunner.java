085dea999381290b3bbaae4121b91edc8629475d
Madhur345 <38024131+Madhur345@users.noreply.github.com>
2019-07-27 12:14:50 +0530

    push

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,8 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="features",
 glue={"stepDefination"},
-plugin={"pretty:target/cucumber-pretty.txt","html:target/cucumber-html-report",
-		"usage:target/cucumber-usage.json","junit:target/cucumber-results.xml"},monochrome = true)
+plugin={"pretty:target/cucumber-pretty.txt","html:target/cucumber-html-report","json:target/cucumber.json",
+		"junit:target/cucumber-results.xml"},monochrome = true)
 public class TestRunner {
 }
 
